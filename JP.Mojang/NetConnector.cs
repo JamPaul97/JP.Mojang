d14747aed51b69323aef1b2f35cb3c19cba7279a
JamPaul <pauloskmantziaris@gmail.com>
2019-10-12 02:54:26 +0300

    Added NetConnector

---
+++ JP.Mojang/NetConnector.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace JP.Mojang
+{
+    public static class NetConnector
+    {
+		private static HttpClient _client = new HttpClient();
+		/// <summary>
+		/// Function to help make the Request to Mojang's Servers
+		/// </summary>
+		/// <param name="obj">The Post object.</param>
+		/// <param name="url">The user to Post.</param>
+		/// <returns></returns>
+		internal static async Task<string> MakeRequest(dynamic obj, string url)
+		{
+			var content = new StringContent(JsonConvert.SerializeObject(obj), System.Text.Encoding.UTF8, "application/json");
+			var response = await _client.PostAsync(url, content);
+			var responseString = await response.Content.ReadAsStringAsync();
+			return responseString;
+		}
+	}
+}
